a960a473b02279958a0245594c4e0e36c45daf13
denglei <244979152@qq.com>
2014-08-27 09:09:55 +0800

    deleted:    common/handlers/main.yml 	deleted:    common/meta/main.yml 	deleted:    common/tasks/main.yml 	deleted:    mysql_delete/meta/main.yml 	deleted:    mysql_delete/tasks/delete.yml 	deleted:    mysql_delete/tasks/main.yml 	deleted:    mysql_delete/vars/main.yml 	deleted:    mysql_install/files/mysql.tar.gz 	deleted:    mysql_install/meta/main.yml 	deleted:    mysql_install/tasks/copy.yml 	deleted:    mysql_install/tasks/delete.yml 	deleted:    mysql_install/tasks/install.yml 	deleted:    mysql_install/tasks/main.yml 	deleted:    mysql_install/templates/install_mysql.sh 	deleted:    mysql_install/templates/my.cnf 	deleted:    mysql_install/templates/mysql_security.sh 	deleted:    mysql_install/templates/mysqld 	deleted:    mysql_install/vars/main.yml

--- mysql_install/mysql_install/templates/mysql_security.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "grant all privileges on *.* to {{ mysql_database_user }}@'{{ ansible_default_ipv4.address }}' identified by '{{ mysql_passwd }}' with grant option;"
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "grant PROCESS,REPLICATION CLIENT ON *.* TO 'zabbix'@'%' identified BY 'zabbix';"
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "flush privileges;"
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "delete from mysql.user where password='';"
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "drop database test;"
-{{ mysql_basedir }}/bin/mysql -h localhost -u root -S {{ mysql_sock }} -e "flush privileges;"
