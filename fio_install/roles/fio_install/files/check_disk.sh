6143c5fc796c35402ac729bcf25a3604a821e909
denglei <244979152@qq.com>
2014-08-31 19:06:09 +0800

    modify exist error

--- fio_install/roles/fio_install/files/check_disk.sh
+++ fio_install/roles/fio_install/files/check_disk.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -v result=`df -h|egrep -Ev '(Filesystem|shm|boot|mapper|/$)'|awk '{print $NF}'|head -n 1`  'BEGIN{if(result=="") {print "/tmp"} else {print result}}'
+awk -v result=`df -h|egrep -Ev '(Filesystem|shm|boot|mapper|devtmpfs|/$)'|awk '{print $NF}'|head -n 1`  'BEGIN{if(result=="") {print "/tmp"} else {print result}}'
