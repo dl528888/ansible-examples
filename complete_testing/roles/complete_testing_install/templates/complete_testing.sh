1c94c0ce6f78aac24afff6480c66faf338585c6f
denglei <244979152@qq.com>
2014-09-02 17:54:20 +0800

    modify sysbench log dir

--- complete_testing/roles/complete_testing_install/templates/complete_testing.sh
+++ complete_testing/roles/complete_testing_install/templates/complete_testing.sh
@@ -15,15 +15,15 @@ cd {{ fio_dir }}
 tpcc_result=`tail -n 1 {{ complete_testing_temp_dir }}/*tpcc*  |awk '{print $1" "$2}'`
 echo "Tpcc reslut is: $tpcc_result" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
 #grep sysbench test result
-cputest=`grep taken {{ complete_testing_temp_dir }}/*sysbench_cputest*`
+cputest=$(grep taken {{ complete_testing_temp_dir }}/*sysbench_cputest*)
 echo "Sysbench cpu_test result is: $cputest" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
-threads=`grep taken {{ complete_testing_temp_dir }}/*sysbench_threads*` 
+threads=$(grep taken {{ complete_testing_temp_dir }}/*sysbench_threads*)
 echo "Sysbench sysbench_threads result is: $threads" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
-mutexxx=`grep taken {{ complete_testing_temp_dir }}/*sysbench_mutexxx*`
+mutexxx=$(grep taken {{ complete_testing_temp_dir }}/*sysbench_mutexxx*)
 echo "Sysbench sysbench_mutexxx result is: $mutexxx" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
-memory=`grep taken {{ complete_testing_temp_dir }}/*sysbench_memory*`
+memory=$(grep taken {{ complete_testing_temp_dir }}/*sysbench_memory*)
 echo "Sysbench sysbench_memory result is: memory" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
 #grep fio test result
-fio=`grep iops {{ complete_testing_temp_dir }}/*fio*`
+fio=$(grep iops {{ complete_testing_temp_dir }}/*fio*)
 echo "Fio result is: $fio" >> {{ complete_testing_dir }}/{{ host }}_complete_testing_result-$time.log
 rm -rf {{ complete_testing_temp_dir }}
